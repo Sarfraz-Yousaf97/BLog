161e312dfcc5b8191ebce37cd9add14a5ef1cc1e
jahan-sarfi14 <muhammadsarfrazyousaf009@gmail.com>
2022-07-20 21:16:08 +0500

    first commit

---
+++ posts/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+from .models import Category,Author,Post
+# Register your models here.
+
+
+admin.site.register(Author)
+admin.site.register(Post)
+admin.site.register(Category)
